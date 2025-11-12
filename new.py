45e85193f1c92ca84516d5c743c3d2feb272dc8b
abhinav-hash007 <tripathiabhinav@gmail.com>
2025-11-13 00:58:28 +0530

    new msg entry

--- new.py
+++ new.py
@@ -18,3 +18,51 @@
 # with open("tasks.txt", "r") as file:
 #     for line in file:
 #         print("Task:", line.strip())
+# 1. re.search()
+# Use: Find the first match anywhere in the string.
+
+# ✅ Best for: Checking if a pattern exists.
+
+# Challenge: Find the first 4-digit year in "He was born in 1998 and graduated in 2020".
+
+# import re
+# line = "He was born in 1998 and graduated in 2020"
+# x = re.search(r"\d{4}", line)
+# if x:
+#     print(f"first yr with 4 letter in it is {x}")
+
+#     2. re.findall()
+# Use: Return all matches as a list.
+
+# ✅ Best for: Extracting multiple items (emails, numbers, words).
+
+# Chall"Abhinav and David went to Delhi"enge: Extract all words starting with capital letters from "Abhinav and David went to Delhi".
+# import re
+# line = "Abhinav and David went to Delhi"
+# x = re.findall(r"[A-Z][a-z]+",line)
+# if x:
+#     print(f"all of the letters with capital alpha are {x}")
+
+# import re
+# num = "I have 1000 cars and 4 all of them are invisible. "
+# x= re.findall(r"[0-9]+", num)
+# for a in x:
+#     print(a)
+# 3. re.sub()
+# Use: Replace all matches with something else.
+
+# ✅ Best for: Masking, cleaning, formatting.
+
+# Challenge: Replace all digits in "My number is 9876543210" with X.
+
+# import re
+# line =  "My number is 9876543210"
+# sub = re.sub("[0-9]","X", line)
+# print(sub)
+import re
+line = "CS50x"
+check = re.search(r"^CS50x$", line)
+if check:
+    print("valid")
+else:
+    print("invalid")
